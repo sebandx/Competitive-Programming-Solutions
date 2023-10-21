8e57d7cbb11014adb08c5c922a5b333a10cc8335
inkay_liu <110617789+sebandx@users.noreply.github.com>
2023-10-21 17:38:15 -0400

    Update and rename main.cpp to CCC '22 S3 - Good Samples.cpp

--- CCC '22 S3 - Good Samples.cpp
+++ CCC '22 S3 - Good Samples.cpp
@@ -3,14 +3,6 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    // tldr the logic of the program is as follows
-    // choose largest good sample sequence size
-    // repeat that sequence
-    // unil k >= q.size()
-    // then you determine the value that you need to set the next value to through the remaining value of k
-    // note that you can fufill the amount of addition good samples required automatically
-    // this is because q.size() > k
-    // and repeat that value for the rest of the sequence, to avoid creating any unexpected good samples 
     ios::sync_with_stdio(0); cin.tie(0);
     ll n; ll m; ll k; cin >> n >> m >> k;
     if (k < n) {
@@ -37,11 +29,6 @@ int main() {
                 q.push_back(x);
             }
         } else {
-            // the first time something enters the else statement
-            // it checks how many remaining good samples need to be created
-            // and thus sets the value accordingly to be repeated for the rest of the sequence
-            // their will never not be enough good samples
-            // because k >= q.size() condition in the above if statement
             ll pos = q.size() - 1 - k;
             a[i] = q[pos];
             k = 0;
