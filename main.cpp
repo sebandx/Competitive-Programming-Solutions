2e752e0293d9ca4f957abf89bf4f603f8c306263
inkay_liu <110617789+sebandx@users.noreply.github.com>
2023-11-04 15:05:19 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,64 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+unordered_map<int, vector<int>> graph;
 
-int main() {
-    string cmd;
-    vector<set<int>> temp = {{}, {6}, {6}, {4,5,6,15}, {3,5,6}, {3,4,6}, {1,2,3,4,5,7}, {6,8}, {7,9}, {8,10,12}, {9,11}, {10,12}, {9,11,13}, {12,14,15}, {13}, {3,13}, {17,18}, {16,18}, {16,17}};
-    vector<set<int>> graph(51);
-
-    for (int i = 0; i < temp.size(); i++) {
-        graph[i] = temp[i];
-    }
-
-    while (cmd != "q") {
-        cin >> cmd;
-        if (cmd == "i") {
-            int x; int y; cin >> x >> y;
-            graph[x].insert(y);
-            graph[y].insert(x);
-        } else if (cmd == "d") {
-            int x; int y; cin >> x >> y;
-            graph[x].erase(y);
-            graph[y].erase(x);
-        } else if (cmd == "n") {
-            int x; cin >> x;
-            cout << graph[x].size() << "\n";
-        } else if (cmd == "f") {
-            int x; cin >> x;
-            set<int> notInclude = graph[x];
-            notInclude.insert(x);
-            set<int> ff;
-            for (auto val : graph[x]) {
-                for (auto val1 : graph[val]) {
-                    if (!notInclude.count(val1)) {
-                        ff.insert(val1);
-                    }
-                }
+bool dfs(int cur, unordered_map<int, int>& visited, unordered_map<int, int>& distance, int start, int target) {
+    visited[cur] = 1;
+    for (auto val : graph[cur]) {
+        if (visited[val] == 1) {
+            if (visited[target] == 1 && visited[start] == 1) {
+                cout << "Yes " << distance[target] - distance[start] - 1 << "\n";
+                return true;
             }
-            cout << ff.size() << "\n";
-        } else if (cmd == "s") {
-            int x; int y; cin >> x >> y;
-            queue<int> q;
-            vector<bool> visited(51, false);
-            vector<int> distance(51, 0);
-            q.push(x);
-            distance[x] = 0;
-            while (!q.empty()) {
-                int current = q.front(); q.pop();
-                for (auto val : graph[current]) {
-                    if (!visited[val]) {
-                        q.push(val);
-                        visited[val] = true;
-                        distance[val] = distance[current] + 1;
-                    }
-                }
-            }
-            if (visited[y]) {
-                cout << distance[y] << "\n";
-            } else {
-                cout << "Not connected" << "\n";
+        } else if (visited[val] == 0) {
+            distance[val] = distance[cur] + 1;
+            if (dfs(val, visited, distance, start, target)) {
+                return true;
             }
         }
+    }
+    visited[cur - 1] = 2;
+    return false;
+}
+
+int main() {
+    int n; cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x; int y; cin >> x >> y;
+        graph[x].push_back(y);
+    }
+    int num1, num2;
+
+    cin >> num1 >> num2;
+    while (num1 != 0 || num2 != 0) {
+        unordered_map<int, int> visited;
+        unordered_map<int, int> distance;
+        for (const auto& pair : graph) {
+            visited[pair.first] = 0;
+            distance[pair.first] = 0;
+        }
+        if (!dfs(num1, visited, distance, num1, num2)) {
+            cout << "No\n";
+        }
+        cin >> num1 >> num2;
     }
     return 0;
 }
